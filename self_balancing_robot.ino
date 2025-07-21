04adbac0e3991cebfc3a9e5ba8f23342dc4ede84
viswavsn81 <viswanathan.vsn@gmail.com>
2025-07-20 18:13:01 -0700

    Trying a commit

--- self_balancing_robot.ino
+++ self_balancing_robot.ino
@@ -1,2 +1,3 @@
 # A simple self-balancing robot using Arduino
 # This code assumes you have a motor driver and an IMU sensor connected to your Arduino
+# Make sure to install the necessary libraries for IMU and motor control
