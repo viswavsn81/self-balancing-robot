e50b1a17ca796335c1b8eb8d92a3518d9910f369
viswavsn81 <viswanathan.vsn@gmail.com>
2025-07-21 13:01:34 -0700

    First working version of self balancing robot

--- MotorDriver.cpp
+++ MotorDriver.cpp
@@ -3,24 +3,40 @@
 
 // Define motor driver pins
 #define AIN1 7
-#define AIN2 8
-#define PWMA 6
-#define BIN1 4
-#define BIN2 9
-#define PWMB 5
+#define PWMA 5
+#define BIN1 8
+#define PWMB 6
 #define STBY 3
 
 void MotorDriver::init(){
-
+    pinMode(AIN1, OUTPUT);
+    pinMode(PWMA, OUTPUT);
+    pinMode(BIN1, OUTPUT);
+    pinMode(PWMB, OUTPUT);
+    pinMode(STBY, OUTPUT);
+    digitalWrite(STBY, HIGH);
 }
 
-void MotorDriver::setMotor(int in1,int in2, int pwm_pin, int speed){
-    // Set the direction pins
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
+void MotorDriver::setMotor(int in1, int pwm_pin, int speed){
     
-    // Set the PWM pin for speed control
-    analogWrite(pwm_pin, speed);
+    if(speed > 0){
+        //Clockwise
+        digitalWrite(in1, HIGH);
+         // Set the PWM pin for speed control
+        analogWrite(pwm_pin, speed);
+    
+    }
+    else{
+        //CCW
+        digitalWrite(in1,LOW);
+         // Set the PWM pin for speed control
+        analogWrite(pwm_pin, -speed);
+    }
+
+    // Set the direction pins
+    
+    
+   
     
     // Ensure standby pin is set to active
     digitalWrite(STBY, HIGH);
@@ -29,8 +45,13 @@ void MotorDriver::setMotor(int in1,int in2, int pwm_pin, int speed){
 
 void MotorDriver::drive(int left_speed, int right_speed){
     // Set left motor
+    setMotor(AIN1, PWMA, -left_speed);
+
+    //Set right motor
+    setMotor(BIN1, PWMB, -right_speed);
+    /*
     if (left_speed > 0) {
-        setMotor(AIN1, AIN2, PWMA, left_speed);
+        
     } else if (left_speed < 0) {
         setMotor(AIN2, AIN1, PWMA, -left_speed);
     } else {
@@ -45,4 +66,12 @@ void MotorDriver::drive(int left_speed, int right_speed){
     } else {
         setMotor(BIN1, BIN2, PWMB, 0);
     }
+    */
+}
+
+void MotorDriver::stop() {
+  analogWrite(PWMA, 0);
+  analogWrite(PWMB, 0);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(BIN1, LOW); 
 }
--- self_balancing_robot.ino
+++ self_balancing_robot.ino
@@ -1,6 +1,65 @@
-# A simple self-balancing robot using Arduino
-# This code assumes you have a motor driver and an IMU sensor connected to your Arduino
-# Make sure to install the necessary libraries for IMU and motor control
 #include <Wire.h>
-#include <MPU6050.h>
+#include "MPU6050.h"
+#include "kalman.h"
+#include "MotorDriver.h"
 #include <PID_v1.h>
+
+#define ACC_LSB_PER_G 8192.0
+#define GYRO_LSB_PER_DPS 65.5
+
+MPU6050 mpu;
+Kalman kalman;
+MotorDriver motors;
+
+// PID variables
+double Setpoint = 0;     // Desired angle (upright)
+double Input = 0;        // Current angle from Kalman
+double Output = 0;       // PID output to motors
+
+double Kp = 8.0;
+double Ki = 0.5;
+double Kd = 1.0;
+
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+unsigned long prevTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.init();
+  motors.init();
+
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255);
+
+  prevTime = millis();
+  Setpoint = 0; // upright
+}
+
+void loop() {
+  int16_t ax, ay, az, gx, gy, gz;
+  mpu.readAccelerometer(ax, ay, az);
+  mpu.readGyroscope(gx, gy, gz);
+
+  float ay_g = (float)ay / ACC_LSB_PER_G;
+  float az_g = (float)az / ACC_LSB_PER_G;
+  float gy_dps = (float)gy / GYRO_LSB_PER_DPS;
+
+  float acc_angle = atan2f(ay_g, az_g) * 180.0f / PI;
+
+  unsigned long now = millis();
+  float dt = (now - prevTime) * 0.001f;
+  prevTime = now;
+
+  float kalman_angle = kalman.getAngle(acc_angle, gy_dps, dt);
+  Input = kalman_angle;
+
+  myPID.Compute();
+  int drive = (int)constrain(Output, -255, 255);
+  motors.drive(drive, drive);
+
+  Serial.print(kalman_angle, 2);
+  Serial.print(',');
+  Serial.println(drive);
+}
